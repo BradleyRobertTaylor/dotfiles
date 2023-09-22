9ba84026e4a626aabd24503367155848c767e3a3
BradleyRobertTaylor <taylorbradleyr@gmail.com>
2023-09-22 14:19:58 -0700

    Eslint disabled when no eslint file in directory

--- nvim/lua/plugins/configs/lsp/null-ls.lua
+++ nvim/lua/plugins/configs/lsp/null-ls.lua
@@ -7,7 +7,9 @@ null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.prettierd,
 		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.diagnostics.eslint_d,
+		null_ls.builtins.diagnostics.eslint_d.with({
+			diagnostics_format = "[eslint] #{m}\n(#{c})",
+		}),
 	},
 	-- for format on save
 	on_attach = function(current_client, bufnr)
