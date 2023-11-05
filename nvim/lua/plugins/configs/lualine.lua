0369db105bc428a1f1c6daf0bda9234e7239f917
BradleyRobertTaylor <taylorbradleyr@gmail.com>
2023-11-04 19:36:12 -0700

    Global status bar disabled

--- nvim/lua/plugins/configs/lualine.lua
+++ nvim/lua/plugins/configs/lualine.lua
@@ -3,6 +3,6 @@ require("lualine").setup({
 		theme = "moonfly",
 		section_separators = { left = "", right = "" },
 		component_separators = { left = "", right = "" },
-		globalstatus = true,
+		-- globalstatus = true,
 	},
 })
