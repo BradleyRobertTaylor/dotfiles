4f8d18c4c7ce3a2757ffdf5aa5929a1e66ff0a8c
BradleyRobertTaylor <taylorbradleyr@gmail.com>
2023-11-19 16:47:54 -0800

    Show hidden files when searching in Telescope

--- nvim/lua/plugins/configs/telescope.lua
+++ nvim/lua/plugins/configs/telescope.lua
@@ -1,6 +1,12 @@
 require("telescope").load_extension("fzf")
 
 require("telescope").setup({
+	-- show hidden files
+	pickers = {
+		find_files = {
+			hidden = true,
+		},
+	},
 	borderchars = {
 		prompt = { "─", " ", " ", " ", "─", "─", " ", " " },
 		results = { " " },
