d163c7f78923b08f7dcada7ac76e93f07c09607d
BradleyRobertTaylor <taylorbradleyr@gmail.com>
2023-12-23 09:46:48 -0800

    Re-order autocomplete sources so lsp come first

--- nvim/lua/plugins/configs/nvim-cmp.lua
+++ nvim/lua/plugins/configs/nvim-cmp.lua
@@ -22,8 +22,8 @@ cmp.setup({
 		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 	}),
 	sources = cmp.config.sources({
-		{ name = "luasnip" },
 		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
 		{ name = "buffer" },
 		{ name = "path" },
 	}),
